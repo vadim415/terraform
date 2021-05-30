a22eb2d58768f6519ace19d3f83d2d08cf7f6c7e
Sergiy <sergey.morenets@gmail.com>
2021-05-30 13:06:54 +0300

    Task #4.2

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 resource "aws_instance" "web" {
   ami           = "AMI-test"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "Our first server instance"
+  }
+}
+
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Description = "Our test VPC"
   }
 }
